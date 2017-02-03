9efd63f807516b6fe29f3150b44970ed52d41275
Eric Covener <covener@gmail.com>
2017-02-03 16:02:16 -0500

    initial version

---
+++ letsencrypt_2_gskit.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -x
+
+# This script copies a letencrypt-downloaded certificate, chain, and key into a 
+# GSKit-compatible CMS file.
+
+DOMAIN=www.misers.org
+IHSROOT=$HOME/ihs90
+
+cd /etc/letsencrypt/live/$DOMAIN
+echo "Fetching root letsencrypt CA"
+wget -q https://letsencrypt.org/certs/isrgrootx1.pem -O $PWD/isrgrootx1.pem
+wget -q https://people.apache.org/~covener/letsencrypt/dst_root.cer -O $PWD/dst.pem
+
+ls -lart
+for ext in rdb kdb crl sth; do
+  rm -f $DOMAIN.$ext
+done
+ls -lart
+
+echo "Convert letsencrypt files to GSKit-compatible P12"
+openssl pkcs12 -export -inkey privkey.pem -in cert.pem  -out $DOMAIN.p12 -certfile chain.pem -name "$DOMAIN" 
+
+echo "Converting p12->cms"
+ls -lart
+$IHSROOT/bin/gskcapicmd -keydb -convert -db $DOMAIN.p12 -new_db $DOMAIN.kdb -new_format cms
+ls -lart
+echo "Stashing password"
+$IHSROOT/bin/gskcapicmd -keydb -stashpw -db $DOMAIN.kdb
+
+echo "Adding root letsencrypt CA"
+$IHSROOT/bin/gskcapicmd -cert -add -db $DOMAIN.kdb -stashed -file /etc/letsencrypt/live/$DOMAIN/isrgrootx1.pem -label isrgrootx1
+$IHSROOT/bin/gskcapicmd -cert -add -db $DOMAIN.kdb -stashed -file /etc/letsencrypt/live/$DOMAIN/dst.pem -label dst
+
+$IHSROOT/bin/gskcapicmd -cert -validate -db $DOMAIN.kdb -stashed  -label "$DOMAIN"
+$IHSROOT/bin/gskcapicmd -cert -list -db $DOMAIN.kdb -stashed 
+
+rm -f $DOMAIN.p12
+
