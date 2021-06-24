e34f6c0ac708116a0483b7afbacded837599b901
Eric Covener <ecovener@us.ibm.com>
2021-06-24 13:54:36 -0400

    add stop/start/restart via mbean

--- pluginutil.sh
+++ pluginutil.sh
@@ -11,6 +11,7 @@ binDir=`dirname $0`
 if [ $# -lt 1 ]; then
   echo "$0 list [ ... wsadmin args ]"
   echo "$0 generate|propagate|propagateKeyring webserver-name webserver-node-name [ ... wsadmin args ]"
+  echo "$0 ping|stop|start|restart webserver-name webserver-node-name [ ... wsadmin args ]"
   exit 1
 fi
 
@@ -47,6 +48,51 @@ fi
 
 echo "Requesting mbean op: $OP"
 
+case $OP in 
+   ping|stop|start)
+(${WAS_HOME}/bin/wsadmin.sh -lang jython "$@" | sed -e 's/wsadmin>//g') <<ENDHEREDOC
+mbean = AdminControl.queryNames("WebSphere:*,process=dmgr,type=WebServer")
+args = '[$WAS_CELL $NODE $WEBSERVER]'
+print AdminControl.invoke(mbean, '$OP', args)
+ENDHEREDOC
+;;
+   restart)
+(${WAS_HOME}/bin/wsadmin.sh -lang jython "$@" | sed -e 's/wsadmin>//g') <<ENDHEREDOC
+import time
+mbean = AdminControl.queryNames("WebSphere:*,process=dmgr,type=WebServer")
+args = '[$WAS_CELL $NODE $WEBSERVER]'
+status = AdminControl.invoke(mbean, 'ping', args)
+
+print "$WEBSERVER is %s" % (status)
+
+if status == "RUNNING":
+    print "Stopping $WEBSERVER"
+    AdminControl.invoke(mbean, 'stop', args)
+
+print "Waiting for $WEBSERVER to stop"
+for i in range(10):
+    if status == "STOPPED":
+        print "$WEBSERVER is stopped, starting"
+        AdminControl.invoke(mbean, 'start', args)
+        break
+    status = AdminControl.invoke(mbean, 'ping', args)
+    sleep(i)
+else:
+   print "Timed out stopping $WEBSERVER"
+
+status = AdminControl.invoke(mbean, 'ping', args)
+for i in range(10):
+    sleep(i)
+    status = AdminControl.invoke(mbean, 'ping', args)
+    if status == "STARTED":
+        print "$WEBSERVER is started"
+        break
+else:
+   print "Timed out starting $WEBSERVER"
+ENDHEREDOC
+;;
+
+   *)
 (${WAS_HOME}/bin/wsadmin.sh -lang jython "$@" | sed -e 's/wsadmin>//g') <<ENDHEREDOC
 mbean = AdminControl.queryNames("WebSphere:*,process=dmgr,type=PluginCfgGenerator")
 args = '[%s/config %s %s %s %s]' % ('$USER_INSTALL_ROOT', '$WAS_CELL', '$NODE', '$WEBSERVER', "false" if "$OP" == "generate" else "")
@@ -54,5 +100,6 @@ types = '[java.lang.String java.lang.String java.lang.String java.lang.String %s
 result = AdminControl.invoke(mbean, '$OP', args, types)
 AdminConfig.save()
 ENDHEREDOC
-
+;;
+esac
 echo "Done"
